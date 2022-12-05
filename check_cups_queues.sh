f3e614e5f8a3b0ec476f04988595e612867685dd
ndias <Nuno.Dias@gmail.com>
2022-12-05 09:27:19 +0000

    Add files via upload
    Version 0.2

--- check_cups_queues.sh
+++ check_cups_queues.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # Check the status of a CUPS queue
-# Nuno.Dias@gmail.com 2022/11/23
-# Version 0.1
+# Nuno.Dias@gmail.com 2022/12/05
+# Version 0.2
 
 RETCODE=0
+DISABLED=""
 
 while read -r queue; do
 
   if lpq -P "$queue" | grep "is not ready" > /dev/null 2>&1; then
-    echo "Error: Queue $queue disabled"
+    DISABLED="$DISABLED $queue"
     RETCODE=2
   fi
 done < <(grep -E "<Printer|<DefaultPrinter" /etc/cups/printers.conf | cut -d" " -f2 | cut -d">" -f1)
 
+if [ "$RETCODE" -eq 2 ]; then 
+  echo "Error: Queue$DISABLED disabled"
+fi
 exit "$RETCODE"
